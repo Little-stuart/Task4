ef4cf5c5c167cc60afe53458df25c17a54b9c77d
Sonali-2204 <patildipu2021@gmail.com>
2025-01-15 18:46:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Smooth Scroll
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const target = document.getElementById(targetId);
+      window.scrollTo({
+        top: target.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
